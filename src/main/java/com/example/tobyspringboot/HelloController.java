1b1b1d8f147d4e6122899a437a8069311b4b1e5d
‍김용빈 <yongbin97@yonsei.ac.kr>
2024-07-14 17:13:08 +0900

    [FEAT]: Section1. Hello 컨트롤러

---
+++ src/main/java/com/example/tobyspringboot/HelloController.java
@@ -0,0 +1,15 @@
+package com.example.tobyspringboot;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello(@RequestParam String name){
+        return "Hello " + name;
+    }
+
+}
