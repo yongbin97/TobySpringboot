ffcb543fab8a110d095464d7d394291f611aa343
Kim Yongbin <80311590+yongbin97@users.noreply.github.com>
2024-07-14 16:42:07 +0900

    init project

---
+++ src/main/java/com/example/tobyspringboot/TobySpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.example.tobyspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TobySpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TobySpringbootApplication.class, args);
+    }
+
+}
