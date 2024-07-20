5ac8507cf8706c765883d1cfb1fb7920ef79360b
Kim Yongbin <80311590+yongbin97@users.noreply.github.com>
2024-07-20 16:14:20 +0900

    Section3-2 Servlet Container

--- src/main/java/com/example/tobyspringboot/TobySpringbootApplication.java
+++ src/main/java/com/example/tobyspringboot/TobySpringbootApplication.java
@@ -1,10 +1,16 @@
 package com.example.tobyspringboot;
 
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.server.WebServer;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+
 public class TobySpringbootApplication {
 
     public static void main(String[] args) {
         System.out.println("Hello Containerless Standalone Application");
-
+        ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
+        WebServer webServer = serverFactory.getWebServer();
+        webServer.start();
     }
 
 }
