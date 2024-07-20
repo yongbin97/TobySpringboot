b357e3080a2aa0b1dcb9cf068e7349f26e45ee78
Kim Yongbin <80311590+yongbin97@users.noreply.github.com>
2024-07-20 17:25:55 +0900

    Section 4-1. Spring Container

--- src/main/java/com/example/tobyspringboot/TobySpringbootApplication.java
+++ src/main/java/com/example/tobyspringboot/TobySpringbootApplication.java
@@ -7,7 +7,7 @@ import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
-import org.springframework.http.HttpHeaders;
+import org.springframework.context.support.GenericApplicationContext;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -17,10 +17,16 @@ import java.io.IOException;
 public class TobySpringbootApplication {
 
     public static void main(String[] args) {
+        // application context 생성
+        GenericApplicationContext applicationContext = new GenericApplicationContext();
+        // Bean에 Controller 등록
+        applicationContext.registerBean(HelloController.class);
+        // Container 초기 -> application context가 빈 Object들을 모두 생성함.
+        applicationContext.refresh();
+
+
         ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
         WebServer webServer = serverFactory.getWebServer(servletContext -> {
-            HelloController helloController = new HelloController();
-
             servletContext.addServlet("frontController", new HttpServlet() {
                 @Override
                 protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
@@ -31,14 +37,11 @@ public class TobySpringbootApplication {
                         // Binding: Query, Body -> Controller
                         String name = req.getParameter("name");
 
+                        HelloController helloController = applicationContext.getBean(HelloController.class);
                         String ret = helloController.hello(name);
 
-                        resp.setStatus(HttpStatus.OK.value());
-                        resp.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_PLAIN_VALUE);
+                        resp.setContentType(MediaType.TEXT_PLAIN_VALUE);
                         resp.getWriter().println(ret);
-                    }
-                    else if (req.getRequestURI().equals("/user")){
-
                     } else {
                         resp.setStatus(HttpStatus.NOT_FOUND.value());
                     }
