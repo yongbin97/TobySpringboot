dae6d90c867bce4ac264e2ab55dcb392d882e17f
Kim Yongbin <80311590+yongbin97@users.noreply.github.com>
2024-07-20 16:04:53 +0900

    Section3-1 Containerless

--- src/main/java/com/example/tobyspringboot/TobySpringbootApplication.java
+++ src/main/java/com/example/tobyspringboot/TobySpringbootApplication.java
@@ -1,13 +1,10 @@
 package com.example.tobyspringboot;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
 public class TobySpringbootApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(TobySpringbootApplication.class, args);
+        System.out.println("Hello Containerless Standalone Application");
+
     }
 
 }
