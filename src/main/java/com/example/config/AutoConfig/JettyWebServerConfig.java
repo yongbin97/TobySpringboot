d888de2d737b628d3c33661dd48d23e9c1a1ef1c
Kim Yongbin <80311590+yongbin97@users.noreply.github.com>
2024-08-25 20:17:41 +0900

    Section 7-1. 스타터와 Jetty 서버 구성 추가

--- src/main/java/com/example/config/AutoConfig/JettyWebServerConfig.java
+++ src/main/java/com/example/config/AutoConfig/JettyWebServerConfig.java
@@ -1,14 +1,15 @@
 package com.example.config.AutoConfig;
 
 import com.example.config.MyAutoConfiguration;
-import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.embedded.jetty.JettyServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
 
 @MyAutoConfiguration
-public class TomcatWebServerConfig {
-    @Bean("tomcatWebServerFactory")
+public class JettyWebServerConfig {
+    @Bean("jettyWebServerFactory")
     public ServletWebServerFactory servletWebServerFactory() {
-        return new TomcatServletWebServerFactory();
+        return new JettyServletWebServerFactory();
     }
+
 }
