bfeb42023858641b34cef1cee35c5faebac26c9f
Kim Yongbin <80311590+yongbin97@users.noreply.github.com>
2024-09-01 15:05:45 +0900

    Section 8-5. 자동 구성 정보 대체하기

--- src/main/java/com/example/config/AutoConfig/TomcatWebServerConfig.java
+++ src/main/java/com/example/config/AutoConfig/TomcatWebServerConfig.java
@@ -2,6 +2,7 @@ package com.example.config.AutoConfig;
 
 import com.example.config.ConditionalMyOnClass;
 import com.example.config.MyAutoConfiguration;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
@@ -10,6 +11,7 @@ import org.springframework.context.annotation.Bean;
 @ConditionalMyOnClass("org.apache.catalina.startup.Tomcat")
 public class TomcatWebServerConfig {
     @Bean("tomcatWebServerFactory")
+    @ConditionalOnMissingBean // 해당 타입의 bean이 등록된 것이 없다면 진행
     public ServletWebServerFactory servletWebServerFactory() {
         return new TomcatServletWebServerFactory();
     }
