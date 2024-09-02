0f656c88b20b1a9c7351e74bf16ccba985b2b5a0
Kim Yongbin <80311590+yongbin97@users.noreply.github.com>
2024-09-02 21:33:13 +0900

    Section 9-3 @Value와 PropertySourcesPlaceholderConfigurer

--- src/main/java/com/example/config/AutoConfig/TomcatWebServerConfig.java
+++ src/main/java/com/example/config/AutoConfig/TomcatWebServerConfig.java
@@ -2,20 +2,23 @@ package com.example.config.AutoConfig;
 
 import com.example.config.ConditionalMyOnClass;
 import com.example.config.MyAutoConfiguration;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
-import org.springframework.core.env.Environment;
 
 @MyAutoConfiguration
 @ConditionalMyOnClass("org.apache.catalina.startup.Tomcat")
 public class TomcatWebServerConfig {
+    @Value("${contextPath}")
+    String contextPath;
+
     @Bean("tomcatWebServerFactory")
     @ConditionalOnMissingBean // 해당 타입의 bean이 등록된 것이 없다면 진행
-    public ServletWebServerFactory servletWebServerFactory(Environment env) {
+    public ServletWebServerFactory servletWebServerFactory() {
         TomcatServletWebServerFactory factory = new TomcatServletWebServerFactory();
-        factory.setContextPath(env.getProperty("contextPath"));
+        factory.setContextPath(contextPath);
         return factory;
     }
 }
--- src/main/java/com/example/tobyspringboot/TobySpringbootApplication.java
+++ src/main/java/com/example/tobyspringboot/TobySpringbootApplication.java
@@ -1,21 +1,10 @@
 package com.example.tobyspringboot;
 
 import com.example.config.MySpringBootApplication;
-import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.core.env.Environment;
 
 @MySpringBootApplication
 public class TobySpringbootApplication {
-    @Bean
-    ApplicationRunner applicationRunner(Environment env) {
-        return args -> {
-            String name = env.getProperty("my.name");
-            System.out.println("my.name: " + name);
-        };
-    }
-
 
     public static void main(String[] args) {
         SpringApplication.run(TobySpringbootApplication.class, args);
